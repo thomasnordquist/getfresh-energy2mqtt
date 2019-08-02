4726e479201a7804161bc21e7fe57fed0b117f2e
Thomas Nordquist <xxnerowingerxx@gmail.com>
2019-08-02 14:54:21 +0200

    Format code with prettier

--- Models.ts
+++ Models.ts
@@ -1,62 +1,62 @@
 export interface LoginResponse {
-	access_token: string;
-	expires_in: number; // probably milliseconds ~10min
-	jti: string;
-	meters: Array<string>;
-	refresh_token: string;
-	scope: "read write" | string;
-	token_type: "bearer" | string;
-	userId: number;
+  access_token: string
+  expires_in: number // probably milliseconds ~10min
+  jti: string
+  meters: Array<string>
+  refresh_token: string
+  scope: 'read write' | string
+  token_type: 'bearer' | string
+  userId: number
 }
 
 export interface UserInfo {
-	bills: Array<any>;
-	email: string;
-	firstName: string;
-	id: number | string;
-	lastName: string;
-	partnerId: "fresh";
-	phone: null;
-	salutation: "Herr" | string;
-	services: [];
-	smartMeters: Array<SmartMeter>;
-	title: null;
+  bills: Array<any>
+  email: string
+  firstName: string
+  id: number | string
+  lastName: string
+  partnerId: 'fresh'
+  phone: null
+  salutation: 'Herr' | string
+  services: []
+  smartMeters: Array<SmartMeter>
+  title: null
 }
 
 export interface Price {
-	value: number;
-	currency: 'EUR' | string;
+  value: number
+  currency: 'EUR' | string
 }
 
 export interface SmartMeter {
-	deinstallationDate: null;
-	installationDate: null | string; // "2018-07-12"
-	meterId: string;
-	meterType: null;
-	operatorId: string;
-	owner: {
-		partnerId: "fresh" | string;
-		id: string;
-	};
-	phaseType: null;
-	readingFrequency: null;
-	tariffs: Array<{
-		validFrom: string; // "2019-01-01" 
-		validUntil: string; // "2030-01-01"
-		kwhUnitGrossPrice: Price;
-		dailyBaseGrossPrice: Price;
-	}>;
+  deinstallationDate: null
+  installationDate: null | string // "2018-07-12"
+  meterId: string
+  meterType: null
+  operatorId: string
+  owner: {
+    partnerId: 'fresh' | string
+    id: string
+  }
+  phaseType: null
+  readingFrequency: null
+  tariffs: Array<{
+    validFrom: string // "2019-01-01"
+    validUntil: string // "2030-01-01"
+    kwhUnitGrossPrice: Price
+    dailyBaseGrossPrice: Price
+  }>
 }
 
 export interface PowerReading {
-	dateTime: string; // "2019-07-24T15:30:01Z"
-	energyReading: number; // 3783.3324891
-	power: number; // 243.32
-	powerPhase1: number; // 25.77
-	powerPhase2: number; // 70.8
-	powerPhase3: number; // 146.75
+  dateTime: string // "2019-07-24T15:30:01Z"
+  energyReading: number // 3783.3324891
+  power: number // 243.32
+  powerPhase1: number // 25.77
+  powerPhase2: number // 70.8
+  powerPhase3: number // 146.75
 }
 
 export type HeaderDict = {
-	[s: string]: string;
-};
+  [s: string]: string
+}
--- index.ts
+++ index.ts
@@ -9,164 +9,175 @@ require('dotenv').config()
 type ReadingCallback = (meter: SmartMeter, reading: PowerReading) => void
 
 class ReadingsMonitor extends EventEmitter {
-	constructor(private apiClient: GetFreshEnergyApiClient, public meter: SmartMeter, public callback?: ReadingCallback) {
-		 super()
-		 this.startPolling()
-	}
+  constructor(private apiClient: GetFreshEnergyApiClient, public meter: SmartMeter, public callback?: ReadingCallback) {
+    super()
+    this.startPolling()
+  }
 
-	public stop() {
-		this.callback = undefined
-	}
+  public stop() {
+    this.callback = undefined
+  }
 
-	private async startPolling() {
-		const userInfo = await this.apiClient.getUserInfo()
-		if (!userInfo) {
-			throw new Error("no user info")
-		}
-		
-		while (this.callback) {
-			try {
-				const consumption = await this.apiClient.getPowerReadings(this.meter.operatorId)
-				consumption.sort((a, b) => new Date(b.dateTime).getTime() - new Date(a.dateTime).getTime())
-				if (consumption[0]) {
-					this.callback(this.meter, consumption[0])
-				}
-			} catch(error) {
-				console.error('failed updating consumption', error)
-			}
+  private async startPolling() {
+    const userInfo = await this.apiClient.getUserInfo()
+    if (!userInfo) {
+      throw new Error('no user info')
+    }
 
-			await new Promise(resolve => setTimeout(resolve, 2000))
-		}
-	}
+    while (this.callback) {
+      try {
+        const consumption = await this.apiClient.getPowerReadings(this.meter.operatorId)
+        consumption.sort((a, b) => new Date(b.dateTime).getTime() - new Date(a.dateTime).getTime())
+        if (consumption[0]) {
+          this.callback(this.meter, consumption[0])
+        }
+      } catch (error) {
+        console.error('failed updating consumption', error)
+      }
+
+      await new Promise(resolve => setTimeout(resolve, 2000))
+    }
+  }
 }
 
 class GetFreshEnergyApiClient {
-	private expiration: number = 0
-	private loginResponse?: LoginResponse
-	private sessionRenewalPromise?: Promise<void>
+  private expiration: number = 0
+  private loginResponse?: LoginResponse
+  private sessionRenewalPromise?: Promise<void>
 
-	constructor(private username: string, private password: string, private partner: 'fresh' | string){}
+  constructor(private username: string, private password: string, private partner: 'fresh' | string) {}
 
-	public createMonitor(meter: SmartMeter, callback: ReadingCallback) {
-		return new ReadingsMonitor(this, meter, callback)
-	}
+  public createMonitor(meter: SmartMeter, callback: ReadingCallback) {
+    return new ReadingsMonitor(this, meter, callback)
+  }
 
-	private setLoginExpired() {
-		this.expiration = 0
-	}
+  private setLoginExpired() {
+    this.expiration = 0
+  }
 
-	private async getValidApiSession(options?: {forLogin?: boolean}): Promise<AxiosInstance> {
-		const clientForLogin = options && options.forLogin
-		if (!clientForLogin) {
-			await this.ensureSessionIsNotExpired()
-		}
+  private async getValidApiSession(options?: { forLogin?: boolean }): Promise<AxiosInstance> {
+    const clientForLogin = options && options.forLogin
+    if (!clientForLogin) {
+      await this.ensureSessionIsNotExpired()
+    }
 
-		const authenticationHeader = !clientForLogin && this.loginResponse ? {
-			'Authorization': 'Bearer ' + (this.loginResponse ? this.loginResponse!.access_token : ''),
-			'Cookie': 'auth=' + qs.stringify(this.loginResponse) + ';' + ' user-has-accepted-cookies=true;'
-		} : {}
+    const authenticationHeader =
+      !clientForLogin && this.loginResponse
+        ? {
+            Authorization: 'Bearer ' + (this.loginResponse ? this.loginResponse!.access_token : ''),
+            Cookie: 'auth=' + qs.stringify(this.loginResponse) + ';' + ' user-has-accepted-cookies=true;',
+          }
+        : {}
 
-		return axios.create({
-			baseURL: 'https://app.getfresh.energy/',
-			headers: {
-				'Accept': 'application/json, text/plain, */*',
-				...authenticationHeader
-			},
-			validateStatus: (status) => {
-				if (status === 401) {
-					this.setLoginExpired()
-				} 
-				return status >= 200 && status < 300; // default
-			},
-		})
-	}
+    return axios.create({
+      baseURL: 'https://app.getfresh.energy/',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        ...authenticationHeader,
+      },
+      validateStatus: status => {
+        if (status === 401) {
+          this.setLoginExpired()
+        }
+        return status >= 200 && status < 300 // default
+      },
+    })
+  }
 
-	private async ensureSessionIsNotExpired() {
-		if (this.sessionRenewalPromise) {
-			return this.sessionRenewalPromise
-		}
+  private async ensureSessionIsNotExpired() {
+    if (this.sessionRenewalPromise) {
+      return this.sessionRenewalPromise
+    }
 
-		if (this.expiration < Date.now()) {
-			console.log("Refresh session")
-			try {
-				this.sessionRenewalPromise = this.login()
-				await this.sessionRenewalPromise
-			} catch(error) {
-				console.error(error)
-				throw new Error('Could not refresh session')
-			}
-		}
-	}
+    if (this.expiration < Date.now()) {
+      console.log('Refresh session')
+      try {
+        this.sessionRenewalPromise = this.login()
+        await this.sessionRenewalPromise
+      } catch (error) {
+        console.error(error)
+        throw new Error('Could not refresh session')
+      }
+    }
+  }
 
-	private async login(): Promise<void> {
-		this.loginResponse = undefined
+  private async login(): Promise<void> {
+    this.loginResponse = undefined
 
-		const credentials = qs.stringify({
-			'grant_type': 'password',
-			'partner': this.partner,
-			'password': this.password,
-			'username': this.username
-		})
+    const credentials = qs.stringify({
+      grant_type: 'password',
+      partner: this.partner,
+      password: this.password,
+      username: this.username,
+    })
 
-		const optionsFormUrlEncoded = {
-			headers: {
-				'Content-type': 'application/x-www-form-urlencoded',
-			}
-		}
+    const optionsFormUrlEncoded = {
+      headers: {
+        'Content-type': 'application/x-www-form-urlencoded',
+      },
+    }
 
-		const apiClient = await this.getValidApiSession({forLogin: true})
-		const response = await apiClient.post('auth/oauth/token', credentials, optionsFormUrlEncoded)
+    const apiClient = await this.getValidApiSession({ forLogin: true })
+    const response = await apiClient.post('auth/oauth/token', credentials, optionsFormUrlEncoded)
 
-		this.loginResponse = response.data
-		this.expiration = this.loginResponse ? Date.now() + this.loginResponse.expires_in  : 0
-	}
+    this.loginResponse = response.data
+    this.expiration = this.loginResponse ? Date.now() + this.loginResponse.expires_in : 0
+  }
 
-	async getUserInfo(): Promise<UserInfo | undefined> {
-		await this.ensureSessionIsNotExpired()
+  async getUserInfo(): Promise<UserInfo | undefined> {
+    await this.ensureSessionIsNotExpired()
 
-		if (!this.loginResponse) {
-			return
-		}
+    if (!this.loginResponse) {
+      return
+    }
 
-		const userId = this.loginResponse.userId
-		try {
-			const apiClient = await this.getValidApiSession()
+    const userId = this.loginResponse.userId
+    try {
+      const apiClient = await this.getValidApiSession()
 
-			const response = await apiClient.get(`user-management/users/${userId}`)
-			return response.data	
-		} catch (error) {
-			throw new Error("Could not load user info")
-		}
-	}
+      const response = await apiClient.get(`user-management/users/${userId}`)
+      return response.data
+    } catch (error) {
+      throw new Error('Could not load user info')
+    }
+  }
 
-	async getPowerReadings(meterId: string): Promise<Array<PowerReading>> {
-		let now = moment.utc().subtract('5', 'seconds').format().replace('Z', '.000Z')
-		const apiClient = await this.getValidApiSession()
-		const response = await apiClient.get(`readings/meters/${meterId}/latest?from=${now}`)
-	
-		return response.data.readings
-	}
+  async getPowerReadings(meterId: string): Promise<Array<PowerReading>> {
+    let now = moment
+      .utc()
+      .subtract('5', 'seconds')
+      .format()
+      .replace('Z', '.000Z')
+    const apiClient = await this.getValidApiSession()
+    const response = await apiClient.get(`readings/meters/${meterId}/latest?from=${now}`)
+
+    return response.data.readings
+  }
 }
 
 const client = connect('mqtt://nodered:1883')
-const freshEnergy = new GetFreshEnergyApiClient(process.env.GETFRESH_USERNAME || '', process.env.GETFRESH_PASSWORD || '', 'fresh')
+const freshEnergy = new GetFreshEnergyApiClient(
+  process.env.GETFRESH_USERNAME || '',
+  process.env.GETFRESH_PASSWORD || '',
+  'fresh'
+)
 
 async function monitorConsumption() {
-	const userInfo = await freshEnergy.getUserInfo()
-	if (!userInfo) {
-		throw Error("Could not load user info")
-	}
+  const userInfo = await freshEnergy.getUserInfo()
+  if (!userInfo) {
+    throw Error('Could not load user info')
+  }
   const firstMeter = userInfo.smartMeters[0]
 
-	const monitor = freshEnergy.createMonitor(firstMeter, (meter, consumption) => {
-		client.publish(`energy_meter/${meter.meterId}`, JSON.stringify(meter))
-		client.publish(`energy_meter/${meter.meterId}/reading`, JSON.stringify(consumption))
-	})
+  const monitor = freshEnergy.createMonitor(firstMeter, (meter, consumption) => {
+    client.publish(`energy_meter/${meter.meterId}`, JSON.stringify(meter))
+    client.publish(`energy_meter/${meter.meterId}/reading`, JSON.stringify(consumption))
+  })
 }
 
 try {
-	monitorConsumption()
-} catch(error) {
-	console.error(error)
-	process.exit(1)
+  monitorConsumption()
+} catch (error) {
+  console.error(error)
+  process.exit(1)
 }
